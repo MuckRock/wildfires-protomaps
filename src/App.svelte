6d66c6f402e50f32ba95912356708154c71e090c
Chris Amico <eyeseast@gmail.com>
2024-01-02 12:31:47 -0500

    Rendering a map. Needs some style refinement and a legend.

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,14 @@
   let container;
   let map;
 
+  const decades = [1980, 1990, 2000, 2010, 2020];
+  const colors = ["#ffffb2", "#fecc5c", "#fd8d3c", "#e31a1c", "#e31a1c"];
+
+  const scale = decades.reduce((m, d, i) => {
+    m = [...m, ["==", ["get", "decade"], d], colors[i]];
+    return m;
+  }, []);
+
   onMount(() => {
     map = new maplibregl.Map({
       container,
@@ -34,17 +42,43 @@
     });
 
     map.once("load", onLoad);
-    map.on("moveend", onMoveEnd);
 
     // for debugging
     window["map"] = map;
   });
 
-  function onLoad(e) {}
-  function onMoveEnd(e) {}
+  function onLoad(e) {
+    const map = e.target;
+
+    const firstSymbolLayer = map
+      .getStyle()
+      .layers.find((layer) => layer.type === "symbol");
+
+    map.addSource("fires", {
+      type: "vector",
+      url: "pmtiles://" + new URL("fires.pmtiles", window.location.href),
+    });
+
+    map.addLayer(
+      {
+        id: "fires",
+        source: "fires",
+        "source-layer": "all-years",
+        type: "fill",
+        layout: {},
+        paint: {
+          "fill-color": ["case", ...scale, "#ddd"],
+          "fill-opacity": 0.5,
+        },
+      },
+      firstSymbolLayer.id
+    );
+  }
 </script>
 
-<div bind:this={container} class="container">Map goes here</div>
+<div bind:this={container} class="container">
+  <noscript> This experience requires JavaScript to function. </noscript>
+</div>
 
 <style>
   .container {
